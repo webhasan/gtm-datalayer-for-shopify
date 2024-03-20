20b895fc7e43eeae52a2d303ffb7b397f5747b41
Md Hasanuzzaman <webhasan24@gmail.com>
2024-03-20 16:29:35 +0600

    Update checkout-webpixel.js

--- checkout-webpixel.js
+++ checkout-webpixel.js
@@ -1,147 +1,120 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-const storeCountryCode = window.localStorage.getItem('shopCountryCode');
 const event_prefix = '';
+const formattedItemId = true;
+const GTM_container_url = 'https://www.googletagmanager.com';
+const GTM_container_id = 'GTM-0000000';
 
-if(window.location.href.includes('/checkouts/cn/')) {
-  // tag manager 
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-000000');
-  // end tag manger
+
+let storeCountryCode = window.localStorage.getItem('shopCountryCode');
+storeCountryCode = storeCountryCode || 'US';
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+if (window.location.href.includes('/checkouts/cn/')) {
+    // tag manager 
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = GTM_container_url + '/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', GTM_container_id);
   
+    analytics.subscribe('page_viewed', (event) => {
+        window.dataLayer.push({
+          event: 'page_view',
+          page_location: event.context.document.location.href,   
+        });
+    });
+    // end tag manger
 
-  // add_payment_info event
-  analytics.subscribe('payment_info_submitted', (event) => {
+
+    // DataLayer Events
+    analytics.subscribe('payment_info_submitted', (event) => ecommerceDataLayer('add_payment_info', event));
+
+    analytics.subscribe('checkout_shipping_info_submitted', (event) => ecommerceDataLayer('add_shipping_info', event));
+
+    analytics.subscribe('checkout_completed', (event) => ecommerceDataLayer('purchase', event));
+
+}
+
+
+async function sha256Hash(value) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(value);
+
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashedValue = hashArray.map(byte => ('00' + byte.toString(16)).slice(-2)).join('');
+    return hashedValue;
+}
+
+async function ecommerceDataLayer(gtm_event_name, event) {  
+    let hash_email;
+    let hash_phone;
+    const phone = event.data?.checkout?.phone;
+    const email = event.data?.checkout?.email;
+
+    if (phone) {
+        hash_phone = await sha256Hash(phone);
+    }
+
+    if (email) {
+        hash_email = await sha256Hash(email);
+    }
+
+
     const customerInfo = {
-      customer: {
-        first_name: event.data?.checkout?.billingAddress?.firstName,
-        last_name: event.data?.checkout?.billingAddress?.lastName,
-        email: event.data?.checkout?.email,
-        phone: event.data?.checkout?.phone,
-        address: event.data?.checkout?.billingAddress
-      }
+        customer: {
+            first_name: event.data?.checkout?.billingAddress?.firstName,
+            last_name: event.data?.checkout?.billingAddress?.lastName,
+            email: email,
+            hash_email: hash_email,
+            phone: phone,
+            hash_phone: hash_phone,
+            address: event.data?.checkout?.billingAddress
+        }
     }
     dataLayer.push(customerInfo);
 
     const dataLayerInfo = {
-      event: event_prefix + 'add_payment_info',
-      ecommerce: {
-        transaction_id: event.data?.checkout?.order?.id,
-        value: event.data?.checkout?.totalPrice?.amount,
-        tax: event.data?.checkout?.totalTax?.amount,
-        shipping: event.data?.checkout?.shippingLine?.price?.amount,
-        currency: event.data?.checkout?.currencyCode,
-        coupon: (event.data?.checkout?.discountApplications || []).map(discount => discount.title).join(','),
-        items: (event.data?.checkout?.lineItems || []).map(item => ({ 
-           item_id: 'shopify_' + storeCountryCode + '_' + (item.variant?.product?.id || '') + '_' + (item.variant?.id || ''),
-           item_name: item.title,
-           coupon: item.discountAllocations?.discountApplication?.title,
-           discount: item.discountAllocations?.amount?.amount,
-           item_variant: item.variant?.title,
-           price: item.variant?.price?.amount,
-           quantity: item.quantity,
-           item_brand: item.variant?.product?.vendor,
-           item_category: item.variant?.product?.type
+        event: event_prefix + gtm_event_name,
+        ecommerce: {
+            transaction_id: event.data?.checkout?.order?.id,
+            value: event.data?.checkout?.totalPrice?.amount,
+            tax: event.data?.checkout?.totalTax?.amount,
+            shipping: event.data?.checkout?.shippingLine?.price?.amount,
+            currency: event.data?.checkout?.currencyCode,
+            coupon: (event.data?.checkout?.discountApplications || []).map(discount => discount.title).join(','),
+            items: (event.data?.checkout?.lineItems || []).map(item => ({
+                item_id: formattedItemId ? 'shopify_' + storeCountryCode + '_' + (item.variant?.product?.id || '') + '_' + (item.variant?.id || '') : item.variant?.product?.id,
+                product_id: item.variant?.product?.id,
+                variant_id: item.variant?.id,
+                item_name: item.title,
+                coupon: item.discountAllocations?.discountApplication?.title,
+                discount: item.discountAllocations?.amount?.amount,
+                item_variant: item.variant?.title,
+                price: item.variant?.price?.amount,
+                quantity: item.quantity,
+                item_brand: item.variant?.product?.vendor,
+                item_category: item.variant?.product?.type
 
-        }))
-      }
+            }))
+        }
     }
 
-    dataLayer.push({ ecommerce: null });
+    dataLayer.push({
+        ecommerce: null
+    });
     dataLayer.push(dataLayerInfo);
 
-     console.log('Event: add_payment_info', Object.assign(dataLayerInfo, customerInfo));
-  });
-
-  // add_shipping_info event
-  analytics.subscribe('checkout_shipping_info_submitted', (event) => {
-
-    const customerInfo = {
-      customer: {
-        first_name: event.data?.checkout?.billingAddress?.firstName,
-        last_name: event.data?.checkout?.billingAddress?.lastName,
-        email: event.data?.checkout?.email,
-        phone: event.data?.checkout?.phone,
-        address: event.data?.checkout?.billingAddress
-      }
-    }
-    dataLayer.push(customerInfo);
-
-    const dataLayerInfo = {
-      event: event_prefix + 'add_shipping_info',
-      ecommerce: {
-        transaction_id: event.data?.checkout?.order?.id,
-        value: event.data?.checkout?.totalPrice?.amount,
-        tax: event.data?.checkout?.totalTax?.amount,
-        shipping: event.data?.checkout?.shippingLine?.price?.amount,
-        currency: event.data?.checkout?.currencyCode,
-        coupon: (event.data?.checkout?.discountApplications || []).map(discount => discount.title).join(','),
-        items: (event.data?.checkout?.lineItems || []).map(item => ({ 
-           item_id: 'shopify_' + storeCountryCode + '_' + (item.variant?.product?.id || '') + '_' + (item.variant?.id || ''),
-           item_name: item.title,
-           coupon: item.discountAllocations?.discountApplication?.title,
-           discount: item.discountAllocations?.amount?.amount,
-           item_variant: item.variant?.title,
-           price: item.variant?.price?.amount,
-           quantity: item.quantity,
-           item_brand: item.variant?.product?.vendor,
-           item_category: item.variant?.product?.type
-
-        }))
-      }
-    }
-
-    dataLayer.push({ ecommerce: null });
-    dataLayer.push(dataLayerInfo);
-
-    console.log('Event: ' + event_prefix + 'add_shipping_info', Object.assign(dataLayerInfo, customerInfo));
-  });
-
-
-
-  // purchase event
-  analytics.subscribe('checkout_completed', (event) => {
-    const customerInfo = {
-      customer: {
-        first_name: event.data?.checkout?.billingAddress?.firstName,
-        last_name: event.data?.checkout?.billingAddress?.lastName,
-        email: event.data?.checkout?.email,
-        phone: event.data?.checkout?.phone,
-        address: event.data?.checkout?.billingAddress
-      }
-    }
-    dataLayer.push(customerInfo);
-
-    const dataLayerInfo = {
-      event: event_prefix + 'purchase',
-      ecommerce: {
-        transaction_id: event.data?.checkout?.order?.id,
-        value: event.data?.checkout?.totalPrice?.amount,
-        tax: event.data?.checkout?.totalTax?.amount,
-        shipping: event.data?.checkout?.shippingLine?.price?.amount,
-        currency: event.data?.checkout?.currencyCode,
-        coupon: (event.data?.checkout?.discountApplications || []).map(discount => discount.title).join(','),
-        items: (event.data?.checkout?.lineItems || []).map(item => ({ 
-           item_id: 'shopify_' + storeCountryCode + '_' + (item.variant?.product?.id || '') + '_' + (item.variant?.id || ''),
-           item_name: item.title,
-           coupon: item.discountAllocations?.discountApplication?.title,
-           discount: item.discountAllocations?.amount?.amount,
-           item_variant: item.variant?.title,
-           price: item.variant?.price?.amount,
-           quantity: item.quantity,
-           item_brand: item.variant?.product?.vendor,
-           item_category: item.variant?.product?.type
-
-        }))
-      }
-    }
-
-    dataLayer.push({ ecommerce: null });
-    dataLayer.push(dataLayerInfo);
-
-     console.log('Event: ' + event_prefix + 'purchase', Object.assign(dataLayerInfo, customerInfo));
-  });
+    console.log('Event: ' + event_prefix + gtm_event_name, Object.assign(dataLayerInfo, customerInfo));
 }
